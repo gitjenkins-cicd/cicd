00937b90e206e18125922ec5aa23abcbd0cbd125
shakeerdcm <shakeer.dcm@gmail.com>
2023-12-26 16:23:13 +0530

    data type

--- pythondevops/datatypes-example.py
+++ pythondevops/datatypes-example.py
@@ -1,2 +1,20 @@
-arn="arn:aws:ec2:us-east-1:123456789012:instance/i-012abcd34efghi56"
-print(arn.split("/")[0].split(":")[3])
+# arn="arn:aws:ec2:us-east-1:123456789012:instance/i-012abcd34efghi56"
+# print(arn.split("/")[0].split(":")[3])
+
+# # Concat
+# str1="Hello"
+# str2="world"
+# res = str1 +","+ str2
+# print(res)
+# print(len(arn))
+
+# Replacing:
+filepath=r"D:/cicd-project1/pythondevops/test.txt"
+with open(filepath, "r") as file:
+    content=file.read()
+    print(content)
+
+updated_content=content.replace("Python", "java")
+print(updated_content)
+with open(filepath, "w") as file:
+    file.write(updated_content)
